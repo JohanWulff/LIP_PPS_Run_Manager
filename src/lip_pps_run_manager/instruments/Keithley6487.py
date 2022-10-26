4dae46002fb4ba79b0719f5b35c92640f607eb43
Johan Wulff <jwulff@lip.pt>
2022-10-26 16:42:24 +0100

    started implementing Keithley Functions

--- src/lip_pps_run_manager/instruments/Keithley6487.py
+++ src/lip_pps_run_manager/instruments/Keithley6487.py
@@ -9,22 +9,30 @@ class Keithley6487(VISADevice):
     def __init__(self, device_name: str, resource_string: str):
         super().__init__(device_type="Keithley6487", device_name=device_name, resource_string=resource_string)
 
-    def set_voltage(self, voltage: float):
-        pass
+    def set_voltage(self, voltage: int):
+        self._VISA_Handle.write(f"SOURCE:VOLTAGE {voltage}")
 
     def get_voltage(self) -> float:
-        pass
+        voltage = self._VISA_Handle.query_ascii_values("MEASURE:VOLTAGE?")
+        return voltage
 
     def get_current(self) -> float:
-        pass
+        current = self._VISA_Handle.query_ascii_values("MEASURE:CURRENT?")
+        return current
 
     def get_cv(self) -> float:
         pass
 
     def set_current_range(self, limit: float) -> bool:
+        if limit not in [10, 50, 500]:
+            raise ValueError("Current limit can only be set to either 10, 50 or 500")
+        self._VISA_Handle.write(f"SOURCE:CURRENT:RANGE {limit}")
         pass
 
     def set_voltage_range(self, limit: float) -> bool:
+        if limit not in [10, 50, 500]:
+            raise ValueError("Voltage limit can only be set to either 10, 50 or 500")
+        self._VISA_Handle.write(f"SOURCE:VOLTAGE:RANGE {limit}")
         pass
 
     def voltage_on(self) -> bool:
